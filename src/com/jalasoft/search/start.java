abc79d2a667dfb677f8e821f65e098fe865ab501
JALASOFT\Gilmar Pozzo <gilmar.pozzo@jalasoft.com>
2018-03-22 11:47:47 -0400

    Package structure and start.java that contains main method.

--- src/com/jalasoft/search/start.java
+++ src/com/jalasoft/search/start.java
@@ -1,6 +1,16 @@
 package com.jalasoft.search;
+/*
+ * Search .java
+ *
+ * Copyright (c) 2018 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ */
 
-public class main {
+public class start {
     public static void main(String [ ] args) {
 
     }

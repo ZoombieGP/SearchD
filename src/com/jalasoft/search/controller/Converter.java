faf2f52a342fe52a095bde109f58e961af7b94a7
Brayan Rosas <brayan_rosas@hotmail.com>
2018-04-05 11:48:19 -0400

    Adding commnets to test classes , change the name of Coverter class

--- src/com/jalasoft/search/controller/Converter.java
+++ src/com/jalasoft/search/controller/Converter.java
@@ -64,4 +64,3 @@ public class Converter {
         return hrSize;
     }
 }
-
--- test/com/jalasoft/search/controller/TestConverter.java
+++ test/com/jalasoft/search/controller/TestConverter.java
@@ -1,21 +1,33 @@
+/*
+ * TestConverter .java
+ *
+ * Copyright (c) 2018 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ */
 package com.jalasoft.search.controller;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
-public class TestConvertor {
-
+public class TestConverter {
+    /**
+     * Verify that the date is convert to string in the format yyyy-dd-mm
+     */
     @Test
     public void convertLongToDate(){
-        String dateExpected="2018-04-04";
+        String dateExpected="2018-05-04";
          Converter converter =new Converter();
          long dateActual = System.currentTimeMillis();
          assertEquals(dateExpected, converter.covertLongToDate(dateActual) );
     }
 
+    /**
+     * Verify that a size in bytes can be converted in KB,MB,GB and TB
+     * , in base to the size
+     */
     @Test
     public void convertBytesToMB(){
         String sizeExpected="5.70 KB";
@@ -25,3 +37,4 @@ public class TestConvertor {
         assertEquals(sizeExpected, converter.formatFileSize(size) );
     }
 }
+

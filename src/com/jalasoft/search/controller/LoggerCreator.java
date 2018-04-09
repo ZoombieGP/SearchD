41dadca8e220af1c2306b7c4e2b12b1dbf223d2a
Brayan Rosas <brayan_rosas@hotmail.com>
2018-04-09 13:56:15 -0400

    Adding comments and copy rigth in LoggerCreator

--- src/com/jalasoft/search/controller/LoggerCreator.java
+++ src/com/jalasoft/search/controller/LoggerCreator.java
@@ -1,3 +1,13 @@
+/*
+ * LoggerCreator.java
+ *
+ * Copyright (c) 2018 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ */
 package com.jalasoft.search.controller;
 
 import java.io.FileNotFoundException;
@@ -6,10 +16,20 @@ import java.util.logging.LogManager;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * Implements the log creation using to configure the log , a log properties file.
+ * @author Brayan Rosas
+ * @version 0.1
+ * @since 04/8/2018
+ */
 public class LoggerCreator {
     private static LoggerCreator loggerCreator;
     private static Logger logger;
 
+    /**
+     * Construct method that read the log.properties
+     * and create a LogManger object to capture logs.
+     */
     private LoggerCreator(){
 
         try (FileInputStream configFile = new FileInputStream("./log.properties")) {
@@ -20,12 +40,16 @@ public class LoggerCreator {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+    /**
+     * Method to implement the Singleton pattern
+     * @return a instance og LoggerCreator class
+     */
     public static LoggerCreator getInstance (){
         if(loggerCreator==null)
             loggerCreator= new LoggerCreator();
         return loggerCreator;
     }
 }
+

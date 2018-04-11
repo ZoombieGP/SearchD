30bc71c87affaedc4b6a0198e758b907550fb915
Brayan Rosas <brayan_rosas@hotmail.com>
2018-04-11 18:58:50 -0400

    Adding the sqlite library and adding commnet to methods

--- src/com/jalasoft/search/common/DBConnection.java
+++ src/com/jalasoft/search/common/DBConnection.java
@@ -1,12 +1,26 @@
+/*
+ * DBConnection.java
+ *
+ * Copyright (c) 2018 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ */
 package com.jalasoft.search.common;
-import org.sqlite.JDBC.*;
-
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * Implements the DB connection using Singleton pattern
+ * @author Brayan Rosas
+ * @version 0.1
+ * @since 04/8/2018
+ */
 public class DBConnection {
     private static DBConnection dbConnection;
     private static Connection con;
@@ -21,19 +35,36 @@ public class DBConnection {
         }
     }
 
+    /**
+     * Return an instance of DBConnection
+     * @return
+     */
     public static  DBConnection getInstance(){
         if(dbConnection==null)
             dbConnection=new DBConnection();
         return dbConnection;
     }
+
+    /**
+     * Connect with the Db and create the tabale that contents the search criteria
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
     private void init() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         con = DriverManager.getConnection("jdbc:sqlite:search.db");
         Statement state=con.createStatement();
-        state.execute("CREATE TABLE IF NOT EXISTS search " +
-                "(ID integer ,criteria varchar(900), " +
-                " PRIMARY KEY(ID);");
+        state.execute("CREATE TABLE SEARCH(\n" +
+                "   ID   INT              NOT NULL,\n" +
+                "   CRITERIA VARCHAR (90),       \n" +
+                "   PRIMARY KEY (ID)\n" +
+                ");");
     }
+
+    /**
+     * Return the current DB connection
+     * @return
+     */
     public  Connection getCon(){
         return con;
     }
--- src/com/jalasoft/search/common/SearchQuery.java
+++ src/com/jalasoft/search/common/SearchQuery.java
@@ -1,12 +1,30 @@
+/*
+ * SearchQuery.java
+ *
+ * Copyright (c) 2018 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ */
 package com.jalasoft.search.common;
-
 
 import java.sql.Connection;
 
+/**
+ * Implements the queries methods for get data from Data Base SEARCH
+ * @author Brayan Rosas
+ * @version 0.1
+ * @since 04/8/2018
+ */
 public class SearchQuery {
 
     private static Connection con;
 
+    /**
+     * Construct method that made the DB connection
+     */
     public SearchQuery(){
 
         con= DBConnection.getInstance().getCon();
